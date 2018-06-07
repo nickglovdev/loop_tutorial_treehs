5e900a3ac93e1bad20070b12e49edb7f06a95236
saintjack <saintjack@Nicholass-MacBook-Air.local>
2018-06-07 08:47:32 -0400

    Added a function to have a computer run through and guess a random number until it finds it.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,30 @@
+
 function randomNumber(upper) {
   return Math.floor( Math.random() * upper ) + 1;
 }
+
+// While loop will run 10 times shooting out 10 random numbers
+document.write('<h2>Randoms Numbers 10 times</h2>');
+var counter = 0;
+while(counter < 10  ){
+    var ranNum = randomNumber(6);
+    document.write(ranNum + '');
+    counter +=1;
+}
+
+// Another While loop that is trying to guess a random number from the function get random number
+document.write('<h2>Computer Find Random Number</h2>')
+function getRandomNumber(upper) {
+  return Math.floor( Math.random() * upper ) + 1;
+}
+var upper = 10000;
+var randomNumber = getRandomNumber(upper)
+var guess;
+var attempts = 0;
+
+while (guess !== randomNumber) {
+  guess = getRandomNumber(upper);
+  attempts += 1;
+}
+document.write('<p>The random number was: ' + randomNumber + '</p>');
+document.write('<p> It took the computer ' + attempts  + ' attempts to get it right</p>');
